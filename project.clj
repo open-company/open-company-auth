ec42a5010b8161742b869a4a20585f13c01632ee
bago2k4 <iacopo@rc0.eu>
2015-09-09 17:27:03 +0200

    add qa env for testing, add needed deps for tests, code checks, cleanup and nice repl

--- project.clj
+++ project.clj
@@ -32,25 +32,87 @@
   ]
 
   :profiles {
-    :dev {
+    ;; QA environment and dependencies
+    :qa {
       :env {
-        :hot-reload true
-        :passphrase "this_is_a_dev_secret"}
+        :hot-reload false
+      }
+      :dependencies [
+        [midje "1.8-alpha1"] ; Example-based testing https://github.com/marick/Midje
+        [ring-mock "0.1.5"] ; Test Ring requests https://github.com/weavejester/ring-mock
+      ]
+      :plugins [
+        [lein-midje "3.2-RC4"] ; Example-based testing https://github.com/marick/lein-midje
+        [jonase/eastwood "0.2.1"] ; Clojure linter https://github.com/jonase/eastwood
+      ]
+    }
+    ;; Dev env and deps
+    :dev [:qa {
+      :env ^:replace {
+        :passphrase "this_is_a_dev_secret"
+        :hot-reload true ; reload code when changed on the file system
+      }
 
       :dependencies [
+        [aprint "0.1.3"] ; Pretty printing in the REPL (aprint thing) https://github.com/razum2um/aprint
         [javax.servlet/servlet-api "3.0-alpha-1"]
-        [ring-mock "0.1.5"]]}
+        [org.clojure/tools.trace "0.7.8"] ; Tracing macros/fns https://github.com/clojure/tools.trace
+      ]
+      :plugins [
+        [lein-bikeshed "0.2.0"] ; Check for code smells https://github.com/dakrone/lein-bikeshed
+        [lein-kibit "0.1.2"] ; Static code search for non-idiomatic code https://github.com/jonase/kibit
+        [lein-checkall "0.1.1"] ; Runs bikeshed, kibit and eastwood https://github.com/itang/lein-checkall
+        [lein-pprint "1.1.2"] ; pretty-print the lein project map https://github.com/technomancy/leiningen/tree/master/lein-pprint
+        [lein-ancient "0.6.7"] ; Check for outdated dependencies https://github.com/xsc/lein-ancient
+        [lein-spell "0.1.0"] ; Catch spelling mistakes in docs and docstrings https://github.com/cldwalker/lein-spell
+        [lein-deps-tree "0.1.2"] ; Print a tree of project dependencies https://github.com/the-kenny/lein-deps-tree
+        [lein-cljfmt "0.3.0"] ; Code formatting https://github.com/weavejester/cljfmt
+        [venantius/ultra "0.3.4"] ; Enhancement's to Leiningen's REPL https://github.com/venantius/ultra
+        [venantius/yagni "0.1.4"] ; Dead code finder https://github.com/venantius/yagni
+      ]
+      ;; REPL config
+      :ultra {
+        :color-scheme :solarized_dark
+        :stacktraces  false
+      }
+      ;; REPL injections
+      :injections [
+        (require '[aprint.core :refer (aprint ap)]
+                 '[clojure.stacktrace :refer (print-stack-trace)]
+                 '[clj-time.format :as t]
+                 '[clojure.string :as s])
+      ]
+    }]
     ;; Production environment
     :prod {
       :env {
         :hot-reload false
-      }}
+      }
+    }
   }
 
   :aliases{
     "start" ["do" "run"] ; start a development server
     "start!" ["with-profile" "prod" "do" "build," "run"] ; start a server in production
     "build" ["do" "clean," "deps," "compile"] ; clean and build code
+    "midje!" ["with-profile" "qa" "midje"] ; run all tests
+    "test!" ["with-profile" "qa" "do" "build," "init-db," "midje"] ; build, init the DB and run all tests
+    "spell!" ["spell" "-n"] ; check spelling in docs and docstrings
+    "bikeshed!" ["bikeshed" "-v" "-m" "120"] ; code check with max line length warning of 120 characters
+    "ancient" ["with-profile" "dev" "do" "ancient" ":allow-qualified," "ancient" ":plugins" ":allow-qualified"] ; check for out of date dependencies
+  }
+  
+  ;; ----- Code check configuration -----
+
+  :eastwood {
+    ;; Dinable some linters that are enabled by default
+    :exclude-linters [:wrong-arity]
+    ;; Enable some linters that are disabled by default
+    :add-linters [:unused-namespaces :unused-private-vars :unused-locals]
+
+    ;; Exclude testing namespaces
+    :tests-paths ["test"]
+    :exclude-namespaces [:test-paths]
   }
 
   ;; ----- Web Application -----
