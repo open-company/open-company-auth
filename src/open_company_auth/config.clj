a3c66380dfeaf9812f5670b6f31e90554b5263cd
Sean Johnson <sean@snootymonkey.com>
2016-05-31 07:52:30 -0400

    Get secrets bucket from env rather than static.

--- src/open_company_auth/config.clj
+++ src/open_company_auth/config.clj
@@ -35,7 +35,8 @@
 (defonce aws-access-key-id (env :aws-access-key-id))
 (defonce aws-secret-access-key (env :aws-secret-access-key))
 
-(def secrets-bucket "open-company-secrets")
-(def secrets-file (if-let [e (env :env)]
-           (str "store-" e)
-           "store"))
+(defonce secrets-bucket (env :aws-secrets-bucket))
+(defonce secrets-file
+  (if-let [e (env :env)]
+    (str "store-" e)
+    "store"))
