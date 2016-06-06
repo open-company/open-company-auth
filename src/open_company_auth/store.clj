c57c3b3a266acab18aca4e0c9293f76707691450
Martin Klepsch <martinklepsch@googlemail.com>
2016-06-06 15:53:38 +0200

    test does-object-exist outside of s3-atom stuff

--- src/open_company_auth/store.clj
+++ src/open_company_auth/store.clj
@@ -43,7 +43,12 @@
   (timbre/info "Got Value" (get-value {:access-key config/aws-access-key-id
                                        :secret-key config/aws-secret-access-key}
                                       config/secrets-bucket
-                                      config/secrets-file)))
+                                      config/secrets-file))
+  (timbre/info "Does object exist?" (s3/does-object-exist {:access-key config/aws-access-key-id
+                                                           :secret-key config/aws-secret-access-key}
+                                                          config/secrets-bucket
+                                                          config/secrets-file)))
+
 
 (defonce db
   (delay
