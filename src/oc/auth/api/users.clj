d7f78a6596f42ef25273107613b1b6c879792d56
Sean Johnson <sean@snootymonkey.com>
2017-01-21 09:27:35 -0500

    Consolidate user API namespaces.

--- src/oc/auth/api/users.clj
+++ src/oc/auth/api/users.clj
@@ -1,10 +1,12 @@
 (ns oc.auth.api.users
-  "Liberator API for users actions (create, email auth, password reset)."
+  "Liberator API for user resources."
   (:require [if-let.core :refer (when-let*)]
             [taoensso.timbre :as timbre]
-            [compojure.core :as compojure :refer (defroutes OPTIONS GET POST)]
+            [compojure.core :as compojure :refer (defroutes OPTIONS GET PATCH POST DELETE)]
             [liberator.core :refer (defresource by-method)]
             [liberator.representation :refer (ring-response)]
+            [schema.core :as schema]
+            [oc.lib.schema :as lib-schema]
             [oc.lib.rethinkdb.pool :as pool]
             [oc.lib.jwt :as jwt]
             [oc.lib.api.common :as api-common]
@@ -36,8 +38,30 @@
           (timbre/info "Failed to auth:" email) 
           false)))))
 
+(defn- update-user [conn {data :data} user-id]
+  (if-let [updated-user (user-res/update-user! conn user-id data)]
+    {:updated-user updated-user}
+    false))
+
+;; ----- Validations -----
+
+(defn- allow-user-and-team-admins [conn {user :user} user-id]
+  (or (= user-id (:user-id user)) ; JWToken user-id matches URL user-id, user accessing themself
+    false)) ; TODO admin of a team the user is on
+
+(defn- processable-patch-req? [conn {data :data} user-id]
+  (if-let [user (user-res/get-user conn user-id)]
+    (try
+      (schema/validate user-res/User (merge user data))
+      true
+      (catch clojure.lang.ExceptionInfo e
+        (timbre/error e "Validation failure of user PATCH request.")
+        false))
+    true)) ; No user for this user-id, so this will 404 after :exists? decision
+
 ;; ----- Resources - see: http://clojure-liberator.github.io/liberator/assets/img/decision-graph.svg
 
+;; A resource for authenticating users by email/pass
 (defresource user-auth [conn]
 
   :allowed-methods [:options :get]
@@ -50,13 +74,17 @@
   :handle-ok (fn [ctx] (user-rep/auth-response (user-res/get-user-by-email conn (-> ctx :request :identity))
                           :email true))) ; respond w/ JWToken and location
 
+;; A resource for creating users by email
 (defresource user-create [conn]
   (api-common/open-company-anonymous-resource config/passphrase)
 
     :allowed-methods [:options :post]
 
     :available-media-types [jwt/media-type]
-    :known-content-type? (fn [ctx] (api-common/known-content-type? ctx user-rep/media-type))
+
+    :known-content-type? (by-method {
+      :options true
+      :get (fn [ctx] (api-common/known-content-type? ctx user-rep/media-type))})
 
     :exists? (fn [ctx] {:existing-user (user-res/get-user-by-email conn (-> ctx :data :email))})
 
@@ -74,6 +102,39 @@
     
     :put! (fn [ctx] (create-user conn (:data ctx))) ; POST ends up handled here so we can have a 409 conflict
     :handle-created (fn [ctx] (user-rep/auth-response (:user ctx) :email true))) ; respond w/ JWToken and location
+
+;; A resource for RUD operations on a particular user
+(defresource user [conn user-id]
+  (api-common/open-company-authenticated-resource config/passphrase)
+
+  :allowed-methods [:options :get :patch :delete]
+
+  :available-media-types [user-rep/media-type]
+  
+  :known-content-type? (by-method {
+                          :options true
+                          :get true
+                          :patch (fn [ctx] (api-common/known-content-type? ctx user-rep/media-type))
+                          :delete true})
+
+  :allowed? (fn [ctx] (allow-user-and-team-admins conn ctx user-id))
+
+  :processable? (by-method {
+    :get true
+    :options true
+    :patch (fn [ctx] (processable-patch-req? conn ctx user-id))
+    :delete true})
+
+  :exists? (fn [ctx] (if-let [user (and (lib-schema/unique-id? user-id) (user-res/get-user conn user-id))]
+                        {:existing-user user}
+                        false))
+
+  :patch! (fn [ctx] (update-user conn ctx user-id))
+  :delete! (fn [_] (user-res/delete-user! conn user-id))
+
+  :handle-ok (by-method {
+    :get (fn [ctx] (user-rep/render-user (:existing-user ctx)))
+    :patch (fn [ctx] (user-rep/render-user (:updated-user ctx)))}))
 
 ;; ----- Routes -----
 
@@ -95,4 +156,8 @@
       ; (OPTIONS "/users/reset/" [] (pool/with-pool [conn db-pool] (password-reset conn)))
       ; (POST "/users/reset" [] (pool/with-pool [conn db-pool] (password-reset conn)))
       ; (POST "/users/reset/" [] (pool/with-pool [conn db-pool] (password-reset conn))))
-    )))
+      ;; user RUD
+      (OPTIONS "/users/:user-id" [user-id] (pool/with-pool [conn db-pool] (user conn user-id)))
+      (GET "/users/:user-id" [user-id] (pool/with-pool [conn db-pool] (user conn user-id)))
+      (PATCH "/users/:user-id" [user-id] (pool/with-pool [conn db-pool] (user conn user-id)))
+      (DELETE "/users/:user-id" [user-id] (pool/with-pool [conn db-pool] (user conn user-id))))))
--- src/oc/auth/app.clj
+++ src/oc/auth/app.clj
@@ -19,8 +19,7 @@
     [oc.auth.components :as components]
     [oc.auth.config :as c]
     [oc.auth.api.entry-point :as entry-point-api]
-    [oc.auth.api.users :as users-api]
-    [oc.auth.api.user :as user-api]))
+    [oc.auth.api.users :as users-api]))
 
 ;; ----- Unhandled Exceptions -----
 
@@ -43,8 +42,7 @@
     (GET "/---error-test---" req (/ 1 0))
     (GET "/---500-test---" req {:status 500 :body "Testing bad things."})
     (entry-point-api/routes sys)
-    (users-api/routes sys)
-    (user-api/routes sys)))
+    (users-api/routes sys)))
 
 ;; ----- System Startup -----
 
