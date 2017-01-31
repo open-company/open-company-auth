396d789b0e1c9ffa4f237d8ff70718f7e1614e89
Sean Johnson <sean@snootymonkey.com>
2017-01-31 11:33:28 -0500

    Include admin flag in user listing.

--- src/oc/auth/representations/user.clj
+++ src/oc/auth/representations/user.clj
@@ -59,8 +59,7 @@
         (item-link user-id)
         (partial-update-link user-id)
         (admin-action-link team-id user-id (:admin user))
-        (remove-link user-id)])
-      (dissoc :admin))))
+        (remove-link user-id)]))))
 
 (defn- user-links
   "HATEOAS links for a user resource"
