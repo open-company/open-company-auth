1a5258dbd40516aca7dfcda2142d3b89bba4331f
Sean Johnson <sean@snootymonkey.com>
2017-03-13 11:17:31 -0400

    Update oc.lib dependency.

--- project.clj
+++ project.clj
@@ -26,7 +26,7 @@
     [buddy/buddy-auth "1.4.1"] ; Authentication for ring https://github.com/funcool/buddy-auth
     [zprint "0.2.16"] ; Pretty-print clj and EDN https://github.com/kkinnear/zprint
     
-    [open-company/lib "0.6.15-ecdd62f"] ; Library for OC projects https://github.com/open-company/open-company-lib
+    [open-company/lib "0.6.16-2255897"] ; Library for OC projects https://github.com/open-company/open-company-lib
     ; In addition to common functions, brings in the following common dependencies used by this project:
     ; defun - Erlang-esque pattern matching for Clojure functions https://github.com/killme2008/defun
     ; if-let - More than one binding for if/when macros https://github.com/LockedOn/if-let
