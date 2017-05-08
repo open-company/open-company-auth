6629c63c8ad4a357c76bdfc7a8b2429aeba65980
Sean Johnson <sean@snootymonkey.com>
2017-05-08 11:11:50 -0400

    Update dependencies.

--- project.clj
+++ project.clj
@@ -12,21 +12,21 @@
   :jvm-opts ^:replace ["-Xms512m" "-Xmx2048m" "-server"]
 
   :dependencies [
-    [org.clojure/clojure "1.9.0-alpha15"] ; Lisp on the JVM http://clojure.org/documentation
+    [org.clojure/clojure "1.9.0-alpha16"] ; Lisp on the JVM http://clojure.org/documentation
     [org.clojure/tools.cli "0.3.5"] ; Command-line parsing https://github.com/clojure/tools.cli
     [http-kit "2.3.0-alpha2"] ; Web client/server http://http-kit.org/
-    [ring/ring-devel "1.6.0-RC3"] ; Web application library https://github.com/ring-clojure/ring
-    [ring/ring-core "1.6.0-RC3"] ; Web application library https://github.com/ring-clojure/ring
+    [ring/ring-devel "1.6.0"] ; Web application library https://github.com/ring-clojure/ring
+    [ring/ring-core "1.6.0"] ; Web application library https://github.com/ring-clojure/ring
     [jumblerg/ring.middleware.cors "1.0.1"] ; CORS library https://github.com/jumblerg/ring.middleware.cors
     [ring-logger-timbre "0.7.5"] ; Ring logging https://github.com/nberger/ring-logger-timbre
-    [compojure "1.6.0-beta3"] ; A concise routing library for Ring/Clojure https://github.com/weavejester/compojure
+    [compojure "1.6.0"] ; A concise routing library for Ring/Clojure https://github.com/weavejester/compojure
     [commons-codec "1.10" :exclusions [[org.clojure/clojure]]] ; Dependency of compojure, ring-core, and midje http://commons.apache.org/proper/commons-codec/
     [org.julienxx/clj-slack "0.5.4"] ; Clojure Slack REST API https://github.com/julienXX/clj-slack
     [buddy "1.3.0"] ; Security library https://github.com/funcool/buddy
     [buddy/buddy-auth "1.4.1"] ; Authentication for ring https://github.com/funcool/buddy-auth
-    [zprint "0.3.2"] ; Pretty-print clj and EDN https://github.com/kkinnear/zprint
+    [zprint "0.4.0"] ; Pretty-print clj and EDN https://github.com/kkinnear/zprint
     
-    [open-company/lib "0.8.2-df1018e"] ; Library for OC projects https://github.com/open-company/open-company-lib
+    [open-company/lib "0.9.4-8954046"] ; Library for OC projects https://github.com/open-company/open-company-lib
     ; In addition to common functions, brings in the following common dependencies used by this project:
     ; defun - Erlang-esque pattern matching for Clojure functions https://github.com/killme2008/defun
     ; if-let - More than one binding for if/when macros https://github.com/LockedOn/if-let
@@ -63,7 +63,7 @@
       :plugins [
         [lein-midje "3.2.1"] ; Example-based testing https://github.com/marick/lein-midje
         [jonase/eastwood "0.2.3"] ; Clojure linter https://github.com/jonase/eastwood
-        [lein-kibit "0.1.3"] ; Static code search for non-idiomatic code https://github.com/jonase/kibit
+        [lein-kibit "0.1.5"] ; Static code search for non-idiomatic code https://github.com/jonase/kibit
       ]
     }
 
@@ -90,7 +90,7 @@
         [lein-spell "0.1.0"] ; Catch spelling mistakes in docs and docstrings https://github.com/cldwalker/lein-spell
         [lein-deps-tree "0.1.2"] ; Print a tree of project dependencies https://github.com/the-kenny/lein-deps-tree
         [venantius/yagni "0.1.4"] ; Dead code finder https://github.com/venantius/yagni
-        [lein-zprint "0.2.2"] ; Pretty-print clj and EDN https://github.com/kkinnear/lein-zprint        
+        [lein-zprint "0.3.0"] ; Pretty-print clj and EDN https://github.com/kkinnear/lein-zprint        
       ]
     }]
     :repl-config [:dev {
