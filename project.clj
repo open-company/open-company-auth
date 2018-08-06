a11177435084bab0b4dc435968ba8bd2b78d70c6
bago2k4 <iacopo@rc0.eu>
2018-08-06 19:16:12 +0200

    Bump up lib version

--- project.clj
+++ project.clj
@@ -50,7 +50,7 @@
     ;; Library for OC projects https://github.com/open-company/open-company-lib
     ;; NB: clj-http pulled in manually
     ;; NB: org.clojure/data.json pulled in manually
-    [open-company/lib "0.16.12-alpha3" :exclusions [clj-http org.clojure/data.json]]
+    [open-company/lib "0.16.12" :exclusions [clj-http org.clojure/data.json]]
     ;; In addition to common functions, brings in the following common dependencies used by this project:
     ;; defun - Erlang-esque pattern matching for Clojure functions https://github.com/killme2008/defun
     ;; if-let - More than one binding for if/when macros https://github.com/LockedOn/if-let
