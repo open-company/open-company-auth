33b32f07985222f6a9ca8f44d16ea176003f0f08
bago2k4 <iacopo@rc0.eu>
2019-08-27 19:09:41 +0200

    Switch to lib version without SNAPSHOT.

--- project.clj
+++ project.clj
@@ -52,7 +52,7 @@
     ;; NB: clj-http pulled in manually
     ;; NB: org.clojure/data.json pulled in manually
 
-    [open-company/lib "0.17.15-SNAPSHOT" :exclusions [clj-http org.clojure/data.json]]
+    [open-company/lib "0.17.15" :exclusions [clj-http org.clojure/data.json]]
     ;; In addition to common functions, brings in the following common dependencies used by this project:
     ;; defun - Erlang-esque pattern matching for Clojure functions https://github.com/killme2008/defun
     ;; if-let - More than one binding for if/when macros https://github.com/LockedOn/if-let
