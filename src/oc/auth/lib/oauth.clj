f91b2d08d6e39b1ceb18016a3eb79b75ad54796b
Calvin Sauer <calvin.j.sauer@gmail.com>
2019-08-30 13:55:58 -0400

    Fix exception thrown by bad Base64 decoding of auth state strings

--- src/oc/auth/lib/oauth.clj
+++ src/oc/auth/lib/oauth.clj
@@ -1,6 +1,8 @@
 (ns oc.auth.lib.oauth
   (:require [clojure.edn :as edn])
-  (:import [java.util Base64]))
+  (:import [java.util Base64]
+           [java.net URLDecoder]
+           [java.nio.charset StandardCharsets]))
 
 (defn encode-state-string
   [data]
@@ -10,7 +12,8 @@
 
 (defn decode-state-string
   [state-str]
-  (let [decoder       (Base64/getUrlDecoder)
-        decoded-bytes (.decode decoder state-str)
+  (let [url-decoded   (URLDecoder/decode state-str (.name StandardCharsets/UTF_8))
+        b64-decoder   (Base64/getDecoder)
+        decoded-bytes (.decode b64-decoder url-decoded)
         decoded-str   (String. decoded-bytes)]
     (edn/read-string decoded-str)))
