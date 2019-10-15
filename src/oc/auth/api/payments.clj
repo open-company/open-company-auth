c63cb1d94efea648cbcbe32813a0204ad77c9a2c
Calvin Sauer <calvin.j.sauer@gmail.com>
2019-10-15 16:36:03 -0400

    Simplify payments api to a single customer resource, and sketch out the customer representation/link-map

--- src/oc/auth/api/payments.clj
+++ src/oc/auth/api/payments.clj
@@ -8,7 +8,9 @@
             [oc.auth.resources.team :as team-res]
             [oc.lib.api.common :as api-common]
             [oc.lib.db.pool :as pool]
-            [oc.lib.user :as lib-user]))
+            [oc.lib.user :as lib-user]
+            [if-let.core :refer (if-let*)]
+            [schema.core :as schema]))
 
 ;; ----- Validations -----
 
@@ -20,6 +22,13 @@
     ((set (:admins team)) user-id)
     false))
 
+(defn- plan-id-from-body
+  [ctx]
+  (if-let* [plan-id (-> ctx :request :body slurp)
+            valid? (schema/validate schema/Str plan-id)]
+    [false {:plan-id plan-id}]
+    true))
+
 ;; ----- Actions -----
 
 (defn create-customer-with-creator-as-contact!
@@ -30,12 +39,14 @@
     {:new-customer (payments-res/create-customer! conn team-id contact)}))
 
 (defn create-new-subscription!
-  [ctx conn team-id plan-id]
-  {:new-subscription (payments-res/start-plan! conn team-id plan-id)})
+  [ctx conn team-id]
+  (when-let [plan-id (:plan-id ctx)]
+    {:new-subscription (payments-res/start-plan! conn team-id plan-id)}))
 
 (defn change-subscription-plan!
-  [ctx conn team-id plan-id]
-  {:updated-subscription (payments-res/change-plan! conn team-id plan-id)})
+  [ctx conn team-id]
+  (when-let [plan-id (:plan-id ctx)]
+    {:updated-subscription (payments-res/change-plan! conn team-id plan-id)}))
 
 (defn cancel-subscription!
   [ctx conn team-id]
@@ -46,7 +57,7 @@
 (defresource customer [conn team-id]
   (api-common/open-company-authenticated-resource config/passphrase)
 
-  :allowed-methods [:options :get :post]
+  :allowed-methods [:options :get :post :put :path :delete]
 
   ;; Media type client accepts
   :available-media-types [mt/payment-customer-media-type]
@@ -56,19 +67,38 @@
   :known-content-type? (by-method {
     :options true
     :get true
-    :post true})
+    :post true
+    :put true
+    :patch true
+    :delete true})
 
   ;; Authorization
   :allowed? (by-method {
     :options true
     :get (fn [ctx] (allow-team-admins conn (:user ctx) team-id))
-    :post (fn [ctx] (allow-team-admins conn (:user ctx) team-id))})
+    :post (fn [ctx] (allow-team-admins conn (:user ctx) team-id))
+    :put (fn [ctx] (allow-team-admins conn (:user ctx) team-id))
+    :patch (fn [ctx] (allow-team-admins conn (:user ctx) team-id))
+    :delete (fn [ctx] (allow-team-admins conn (:user ctx) team-id))
+    })
 
   ;; Validations
   :processable? (by-method {
     :options true
     :get true
-    :post (fn [_] (nil? (payments-res/get-customer conn team-id)))})
+    :post (fn [ctx] (nil? (:existing-customer ctx)))
+    :put (fn [ctx] (nil? (-> ctx :existing-customer :subscription)))
+    :patch (fn [ctx] (some? (-> ctx :existing-customer :subscription)))
+    :delete (fn [ctx] (some? (-> ctx :existing-customer :subscription)))
+    })
+
+  :malformed? (by-method {
+    :options false
+    :get false
+    :post false
+    :put plan-id-from-body
+    :patch plan-id-from-body
+    :delete plan-id-from-body})
 
   ;; Existentialism
   :exists? (fn [ctx] (if-let [customer (payments-res/get-customer conn team-id)]
@@ -77,52 +107,13 @@
 
   ;; Actions
   :post! (fn [ctx] (create-customer-with-creator-as-contact! ctx conn team-id))
-
-  ;; Responses
-  :handle-ok (fn [ctx] (let [customer (or (:existing-customer ctx)
-                                          (:new-customer ctx))]
-                          (payments-rep/render-customer customer)))
-  )
-
-(defresource subscription [conn team-id plan-id]
-  (api-common/open-company-authenticated-resource config/passphrase)
-
-  :allowed-methods [:options :put :patch :delete]
-
-  ;; Media type client accepts
-  :available-media-types [mt/payment-subscription-media-type]
-  :handle-not-acceptable (api-common/only-accept 406 mt/payment-subscription-media-type)
-
-  ;; Media type client sends
-  :known-content-type? (by-method {
-    :options true
-    :get true
-    :post true})
-
-  ;; Authorization
-  :allowed? (by-method {
-    :options true
-    :get (fn [ctx] (allow-team-admins conn (:user ctx) team-id))
-    :post (fn [ctx] (allow-team-admins conn (:user ctx) team-id))})
-
-  ;; Validations
-  :processable? (by-method {
-    :options true
-    :put true
-    :patch true
-    :delete true})
-
-  ;; Actions
-  :put! (fn [ctx] (create-new-subscription! ctx conn team-id plan-id))
-  :path! (fn [ctx] (change-subscription-plan! ctx conn team-id plan-id))
+  :put! (fn [ctx] (create-new-subscription! ctx conn team-id))
+  :patch! (fn [ctx] (change-subscription-plan! ctx conn team-id))
   :delete! (fn [ctx] (cancel-subscription! ctx conn team-id))
 
-
   ;; Responses
-  :handle-ok (fn [ctx] (let [subscription (or (:new-subscription ctx)
-                                              (:updated-subscription ctx)
-                                              (:canceled-subscription ctx))]
-                         (payments-rep/render-subscription subscription)))
+  :handle-ok (fn [ctx] (let [customer (payments-res/get-customer conn team-id)]
+                          (payments-rep/render-customer team-id customer)))
   )
 
 ;; ----- Routes -----
@@ -131,5 +122,4 @@
   (let [db-pool (-> sys :db-pool :pool)]
     (compojure/routes
      (ANY "/teams/:team-id/customer" [team-id] (pool/with-pool [conn db-pool] (customer conn team-id)))
-     (ANY "/teams/:team-id/customer/:plan-id" [team-id plan-id] (pool/with-pool [conn db-pool] (subscription conn team-id plan-id)))
      )))
--- src/oc/auth/representations/payments.clj
+++ src/oc/auth/representations/payments.clj
@@ -1,31 +1,43 @@
 (ns oc.auth.representations.payments
-  (:require [cheshire.core :as json]))
+  (:require [cheshire.core :as json]
+            [defun.core :refer (defun)]
+            [oc.lib.hateoas :as hateoas]
+            [oc.auth.representations.media-types :as mt]))
 
 (def customer-representation-props
   [:id :email :full-name :subscription])
 
-(def subscription-representation-props
-  [:id
-   :current-period-start
-   :current-period-end
-   :trial-start
-   :trial-end
-   :status
-   :current-plan
-   :available-plans
-   :usage
-   :item
-   ])
+(defun customer-url
+  ([team-id :guard string?] (str "/teams/" team-id "/customer"))
+  ([team :guard map?] (str "/teams/" (:team-id team) "/customer")))
+
+(defn- self-link
+  [team-id]
+  (hateoas/self-link (customer-url team-id) {:content-type mt/payment-customer-media-type}))
+
+(defn- create-customer-link
+  [team-id]
+  (hateoas/create-link (customer-url team-id) {:content-type mt/payment-customer-media-type}))
+
+(defn- create-subscription-link
+  [team-id]
+  (hateoas/update-link (customer-url team-id) {:content-type mt/payment-customer-media-type}))
+
+(defn- change-plan-link
+  [team-id]
+  (hateoas/partial-update-link (customer-url team-id) {:content-type mt/payment-customer-media-type}))
+
+(defn- cancel-subscription-link
+  [team-id]
+  (hateoas/delete-link (customer-url team-id) {:content-type mt/payment-customer-media-type}))
 
 (defn render-customer
-  [customer]
+  [team-id customer]
   (-> customer
       (select-keys customer-representation-props)
-      json/generate-string))
-
-
-(defn render-subscription
-  [sub]
-  (-> sub
-      (select-keys subscription-representation-props)
+      (assoc :links [(self-link team-id)
+                     (create-customer-link team-id)
+                     (create-subscription-link team-id)
+                     (change-plan-link team-id)
+                     (cancel-subscription-link team-id)])
       json/generate-string))
