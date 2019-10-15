49bdf0a2d9bf97f41966ddbacb095ae5cf002414
Calvin Sauer <calvin.j.sauer@gmail.com>
2019-10-15 11:23:43 -0400

    Fill in prismatic schema definitions for payments

--- src/oc/auth/resources/payments.clj
+++ src/oc/auth/resources/payments.clj
@@ -6,7 +6,42 @@
 
 ;; ----- Schema -----
 
-(def PaymentContact
+(def Plan
+  {:id       schema/Str
+   :amount   schema/Int
+   :nickname schema/Str
+   :currency (schema/enum "usd")
+   :active   schema/Bool
+   :interval (schema/enum "month" "annual")
+   })
+
+(def UsageSummary
+  {:seats schema/Int})
+
+(def SubscriptionItem
+  {:id schema/Str})
+
+(def Subscription
+  {:id                   schema/Str
+   :current-period-start schema/Int
+   :current-period-end   schema/Int
+   :trial-start          (schema/maybe schema/Int)
+   :trial-end            (schema/maybe schema/Int)
+   :status               (schema/enum "trialing" "active")
+   :current-plan         Plan
+   :available-plans      [Plan]
+   :usage                UsageSummary
+   :item                 SubscriptionItem
+   })
+
+(def Customer
+  {:id        schema/Str
+   :email     schema/Str
+   :full-name schema/Str
+   (schema/optional-key :subscription) (schema/maybe Subscription)
+   })
+
+(def CustomerContact
   {:email     schema/Str
    :full-name schema/Str})
 
@@ -17,27 +52,27 @@
   (-> (team/get-team conn team-id)
       :stripe-customer-id))
 
-(schema/defn ^:always-validate create-customer!
-  [conn team-id :- (:team-id team/Team) contact :- PaymentContact]
+(schema/defn ^:always-validate create-customer! :- Customer
+  [conn team-id :- (:team-id team/Team) contact :- CustomerContact]
   {:pre [(db-common/conn? conn)]}
   (let [stripe-meta {"carrotTeamId" team-id}
         customer    (stripe/create-stripe-customer! contact stripe-meta)
         customer-id (:id customer)]
     (team/update-team! conn team-id {:stripe-customer-id customer-id})))
 
-(schema/defn ^:always-validate get-customer
+(schema/defn ^:always-validate get-customer :- Customer
   [conn team-id :- (:team-id team/Team)]
   {:pre [(db-common/conn? conn)]}
   (let [customer-id (get-customer-id conn team-id)]
     (stripe/customer-info customer-id)))
 
-(schema/defn ^:always-validate change-plan!
+(schema/defn ^:always-validate change-plan! :- SubscriptionItem
   [conn team-id :- (:team-id team/Team) new-plan-id]
   {:pre [(db-common/conn? conn)]}
   (let [customer-id (get-customer-id conn team-id)]
     (stripe/change-plan! customer-id new-plan-id)))
 
-(schema/defn ^:always-validate cancel-subscription!
+(schema/defn ^:always-validate cancel-subscription! :- Subscription
   [conn team-id :- (:team-id team/Team)]
   {:pre [(db-common/conn? conn)]}
   (let [customer-id (get-customer-id conn team-id)]
