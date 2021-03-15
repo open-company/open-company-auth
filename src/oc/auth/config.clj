adfdb867e1f64acfbc4a2619eb1a107904658cf0
bago2k4 <iacopo@rc0.eu>
2021-03-16 00:45:47 +0100

    Fix log-level type, make sure it's a keyword when it's retrieved from env.

--- src/oc/auth/config.clj
+++ src/oc/auth/config.clj
@@ -20,7 +20,7 @@
 
 ;; ----- Logging (see https://github.com/ptaoussanis/timbre) -----
 
-(defonce log-level (or (env :log-level) :info))
+(defonce log-level (if-let [log-level (env :log-level)] (keyword log-level) :info))
 
 ;; ----- Sentry -----
 
